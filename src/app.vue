00acd5f8aa0d9c19209668abcb51e1248c06db5c
carry1111 <529709962@qq.com>
2019-04-08 17:40:50 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,35 @@
+<style scoped lang="less">
+    // @import 'styles/common.less';
+    .msg-wrapper{
+        width: 100px;
+        height: 100px;
+    }
+    .account{
+        font-size: 12px;
+    }
+</style>
+<template>
+    <div class="msg-wrapper">
+        <span>hello!</span>
+        <span class="account">{{data.name}}</span>
+    </div>
+</template>
+<script>
+    export default {
+        name:'Msg',
+        data () {
+            return {}
+        },
+        props:{
+            data:{
+                type:Object
+            }
+        },
+        methods: {
+ 
+        },
+        created(){
+            let that = this;
+        }
+    }
+</script>
